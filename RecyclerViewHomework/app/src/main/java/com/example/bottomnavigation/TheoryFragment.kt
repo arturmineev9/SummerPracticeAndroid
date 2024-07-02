86f97feb63438fffb9393cc9cd2f8c70c83ce39e
Arthur <arturmineev9@mail.ru>
2024-07-03 02:14:21 +0300

    Создание DesignedFragment

--- RecyclerViewHomework/app/src/main/java/com/example/bottomnavigation/TheoryFragment.kt
+++ RecyclerViewHomework/app/src/main/java/com/example/bottomnavigation/TheoryFragment.kt
@@ -3,6 +3,7 @@ package com.example.bottomnavigation
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
 import com.example.bottomnavigation.databinding.FragmentTheoryBinding
@@ -29,8 +30,15 @@ class TheoryFragment : Fragment(R.layout.fragment_theory) {
             adapter = TheoryAdapter(
                 list = TheoryRepository.items,
                 glide = Glide.with(this@TheoryFragment),
-                onClick = {
-                    Snackbar.make(root, it.imageUrl, Snackbar.LENGTH_LONG).show()
+                onClick = { item ->
+                    val bundle = Bundle()
+                    bundle.putInt("ARG_ID", item.id)
+                    findNavController().navigate(
+                        resId = R.id.action_theoryFragment_to_detailedFragment,
+                        args = DetailedFragment.bundle(
+                            theoryID = item.id
+                        )
+                    )
                 }
             )
 
